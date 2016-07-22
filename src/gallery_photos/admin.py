2bb74a552e2658ed52b553317ef51dff85cabef8
Simon Gawlik <sgawlik@gmail.com>
2016-07-22 18:52:44 -0400

    further updates to admin view

--- src/gallery_photos/admin.py
+++ src/gallery_photos/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Photo
 
-admin.site.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+	list_display = ["__str__", "timestamp_posted", "timestamp_updated"]
+	class Meta:
+		model = Photo
+
+admin.site.register(Photo, PhotoAdmin)
